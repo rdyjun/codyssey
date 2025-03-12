af1c0afd12dbce4badbc94810e3d963239f49a4f
rdyjun <rdyjun00@gmail.com>
2025-03-12 15:55:52 +0900

    feat: add problem log writing

--- week-1/main.py
+++ week-1/main.py
@@ -1,14 +1,24 @@
 print('Hello Mars')
 
+isProblem = False
+
 try:
-    f = open('week-1/mission_computer_main.log', 'r', encoding = 'utf-8');
-    # print(f.read()); // 정방향
-    log_array = f.read().strip().split('\n')
-    print(log_array[0])
+    problem_log_file = open('week-1/problem.log', 'w', encoding = 'utf-8');
+    logs = open('week-1/mission_computer_main.log', 'r', encoding = 'utf-8').read();
+    log_array = logs.strip().split('\n')
     del log_array[0]
 
+    # for log in log_array: # 정방향
+    #     print(log)
     for log in reversed(log_array):
         print(log)
+
+    for log in log_array:
+        if 'explosion' in log:
+            isProblem = True
+        if isProblem:
+            problem_log_file.write(log + '\n')
+
 except FileNotFoundError:
     print("파일을 찾을 수 없습니다.")
 except UnicodeDecodeError:
