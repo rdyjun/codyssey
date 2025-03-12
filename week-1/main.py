610fba10acb087e1e4ae13c737a3e67f5d0ac628
rdyjun <rdyjun00@gmail.com>
2025-03-12 15:22:07 +0900

    refactor: remove os

--- week-1/main.py
+++ week-1/main.py
@@ -1,10 +1,5 @@
-import os
-
-script_dir = os.path.dirname(os.path.abspath(__file__))
-log_path = os.path.join(script_dir, 'Codyssey 프로젝트 현황 로그.log')
-
 try:
-    f = open(log_path, 'r', encoding = 'utf-8');
+    f = open('week-1/mission_computer_main.log', 'r', encoding = 'utf-8');
     print(f.read());
 except FileNotFoundError:
     print("파일을 찾을 수 없습니다.")
