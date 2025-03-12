b6d91cfeca23f51a4657431056de72117c917dc9
rdyjun <rdyjun00@gmail.com>
2025-03-12 15:41:33 +0900

    refactor: log reverse print

--- week-1/main.py
+++ week-1/main.py
@@ -1,6 +1,12 @@
 try:
     f = open('week-1/mission_computer_main.log', 'r', encoding = 'utf-8');
-    print(f.read());
+    # print(f.read()); // 정방향
+    log_array = f.read().strip().split('\n')
+    print(log_array[0])
+    del log_array[0]
+
+    for log in reversed(log_array):
+        print(log)
 except FileNotFoundError:
     print("파일을 찾을 수 없습니다.")
 except UnicodeDecodeError:
