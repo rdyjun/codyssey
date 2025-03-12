c7e89e341c07f3c82f865b7991edf46398a7dd50
rdyjun <rdyjun00@gmail.com>
2025-03-12 14:46:39 +0900

    refactor: add exception handling

--- week-1/main.py
+++ week-1/main.py
@@ -3,5 +3,12 @@ import os
 script_dir = os.path.dirname(os.path.abspath(__file__))
 log_path = os.path.join(script_dir, 'Codyssey 프로젝트 현황 로그.log')
 
-f = open(log_path, 'r', encoding='utf-8');
-print(f.read());
+try:
+    f = open(log_path, 'r', encoding = 'utf-8');
+    print(f.read());
+except FileNotFoundError:
+    print("파일을 찾을 수 없습니다.")
+except UnicodeDecodeError:
+    print("파일을 UTF-8로 디코딩할 수 없습니다. 다른 인코딩을 사용해 보세요.")
+except Exception as e:
+    print(f"예상치 못한 오류가 발생했습니다: {e}")
