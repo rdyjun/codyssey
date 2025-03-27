9955a90d5e931629d071848b811ac699fe46d0ad
rdyjun <rdyjun00@gmail.com>
2025-03-27 12:48:59 +0900

    feat: add log write at the file

--- mission-3/mars_mission_computer.py.py
+++ mission-3/mars_mission_computer.py.py
@@ -8,20 +8,22 @@ class EnvValues:
     mars_base_internal_co2: float       # 0.02 ~ 0.1
     mars_base_internal_oxygen: int      # 4 ~ 7
 
-    internal_temperature_range: tuple[int, int] = (18, 30)
-    external_temperature_range: tuple[int, int] = (0, 21)
-    internal_humidity_range: tuple[int, int] = (50, 60)
-    external_illuminance_range: tuple[int, int] = (500, 715)
-    internal_co2_range: tuple[int, int] = (0.02, 0.1)
-    internal_oxygen_range: tuple[int, int] = (4, 7)
+    _internal_temperature_range: tuple[int, int] = (18, 30)
+    _external_temperature_range: tuple[int, int] = (0, 21)
+    _internal_humidity_range: tuple[int, int] = (50, 60)
+    _external_illuminance_range: tuple[int, int] = (500, 715)
+    _internal_co2_range: tuple[int, int] = (0.02, 0.1)
+    _internal_oxygen_range: tuple[int, int] = (4, 7)
 
     def random(self):
-        self.mars_base_internal_temperature = random.randint(self.internal_temperature_range)
-        self.mars_base_external_temperature = random.randint(self.external_temperature_range)
-        self.mars_base_internal_humidity = random.randint(self.internal_humidity_range)
-        self.mars_base_external_illuminance = random.randint(self.external_illuminance_range)
-        self.mars_base_internal_co2 = random.uniform(self.internal_co2_range)
-        self.mars_base_internal_oxygen = random.randint(self.internal_oxygen_range)
+        self.mars_base_internal_temperature = random.randint(*self._internal_temperature_range)
+        self.mars_base_external_temperature = random.randint(*self._external_temperature_range)
+        self.mars_base_internal_humidity = random.randint(*self._internal_humidity_range)
+        self.mars_base_external_illuminance = random.randint(*self._external_illuminance_range)
+        self.mars_base_internal_co2 = random.uniform(*self._internal_co2_range)
+        self.mars_base_internal_oxygen = random.randint(*self._internal_oxygen_range)
+    def get_items(self):
+        return self.__dict__.items()
 
 class DummySensor:
     env_values: EnvValues
@@ -31,6 +33,12 @@ class DummySensor:
         self.env_values.random()
 
     def get_env(self):
+        with open('./mission-3/env.log', 'w') as f:
+            for field_name, value in self.env_values.get_items():
+                if field_name.startswith('_'):  # private 속성 제외
+                    continue
+
+                f.write(f"{field_name}: {value}\n")
         return self.env_values
 
 class MarsMissionComputer:
