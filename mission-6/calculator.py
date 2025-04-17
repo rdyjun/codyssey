f771bfd45326cf3a22fd33ad048c2c62f4e56b5f
rdyjun <rdyjun00@gmail.com>
2025-04-17 14:26:57 +0900

    feat: add comment to code

--- mission-6/calculator.py
+++ mission-6/calculator.py
@@ -20,25 +20,26 @@ from number_button import (
 class CalculatorWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Calculator")
-        self.setFixedSize(350, 500)
-        self.init_ui()
+        self.setWindowTitle("Calculator")   # 윈도우 창 이름 설정
+        self.setFixedSize(350, 500)         # 윈도우 크기 설정
+        self.init_ui()                      # 실제 UI 구성
 
     def init_ui(self):
-        self.setStyleSheet("background-color: #18191a;")
+        self.setStyleSheet("background-color: #18191a;")    # 배경 색
 
-        central_widget = QWidget(self)
-        self.setCentralWidget(central_widget)
-        main_layout = QVBoxLayout()
-        central_widget.setLayout(main_layout)
+        central_widget = QWidget(self)                      # 위젯 생성, 이 위젯이 있어야 레이아웃 배치가 가능
+        self.setCentralWidget(central_widget)               # 위젯을 중앙에 배치
+
+        main_layout = QVBoxLayout()                         # 메인 레이아웃 생성 - 수직 레이아웃
+        central_widget.setLayout(main_layout)               # 중앙 위젯에 레이아웃 설정
 
         # Display
-        self.display = QLineEdit("4,256,545")
-        self.display.setAlignment(Qt.AlignRight)
-        self.display.setReadOnly(True)
-        self.display.setFont(QFont("Arial", 36, QFont.Bold))
+        self.display = QLineEdit("4,256,545")               # 초기 값 설정
+        self.display.setAlignment(Qt.AlignRight)            # 값 오른쪽 정렬
+        self.display.setReadOnly(True)                      # 읽기 전용으로 설정 (계산기기 때문)
+        self.display.setFont(QFont("Arial", 36, QFont.Bold))# 폰트 설정
         self.display.setStyleSheet("background: transparent; color: white; border: none; padding: 10px;")
-        main_layout.addWidget(self.display)
+        main_layout.addWidget(self.display)                 # 레이아웃에 배치
 
         # 버튼 레이아웃
         btn_layout = QGridLayout()
@@ -70,7 +71,7 @@ class CalculatorWindow(QMainWindow):
             btn_layout.addWidget(button, button.row, button.column, button.row_span, button.column_span)
 
 if __name__ == "__main__":
-    app = QApplication([])
-    window = CalculatorWindow()
-    window.show()
-    sys.exit(app.exec_())
+    app = QApplication([])      # PyQT로 GUI를 생성하기 위한 QApplication 객체 생성
+    window = CalculatorWindow() # 구현한 GUI 생성
+    window.show()               # 생성한 GUI를 화면에 표시
+    sys.exit(app.exec_())       # 프로그램 종료 전까지 계속 실행
